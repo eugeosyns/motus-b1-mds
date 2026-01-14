1a4a6c2804696cbe91dbac5110f117b8fabc465f
eugeosyns <thomasleo723@gmail.com>
2026-01-14 14:37:42 +0100

    fonction flécher

--- script.js
+++ script.js
@@ -3,28 +3,62 @@ const dictionnaire = ["login","cache","input","debug","stack","array","param","t
 /*choix du mot aleatoire*/
 const motAleatoire = dictionnaire[Math.floor(Math.random() * dictionnaire.length)];
 
-/*fonction qui renvoie un tableau avec chaque lettre du mot*/
-function decomposerMot(mot) {
-    mot = mot.toUpperCase()
-  return mot.split('');
-
-}
-
-const lettres = decomposerMot(motAleatoire);
-let nbEssaie = 0;
-
-function recupererMot() {
+const recupererMot = () => {
   const input = document.getElementById('monInput');
-  const mot = input.value;
-  return mot;
-}
+  return input.value;
+};
 
+/* fonction qui renvoie un tableau avec chaque lettre du mot */
+const decomposerMot = (mot) => {
+  return mot.toUpperCase().split('');
+};
 
+const verifierEssai = (essaie, lettres, nouvelleLigne) => {
+    let toutCorrect = false;
+
+    essaie.forEach((lettre, i) => {
+        const cellule = document.createElement('td');
+        cellule.textContent = lettre;
+
+        if (lettre === lettres[i]) {
+            cellule.classList.add('correct');
+            console.log(lettre + " - Bonne position");
+            toutCorrect = true;
+        } else if (lettres.includes(lettre)) {
+            cellule.classList.add('present');
+            console.log(lettre + " - Mauvaise position mais dans le mot");
+            toutCorrect = false;
+        } else {
+            cellule.classList.add('absent');
+            console.log(lettre + " - Pas dans le mot");
+            toutCorrect = false;
+        }
+
+        nouvelleLigne.appendChild(cellule);
+    });
+    if (toutCorrect == true) {
+        messageDiv.textContent = "🎉 Bravo ! Vous avez trouvé le mot !";
+        bouton.disabled = true;
+        input.disabled = true;
+
+    } else {
+        messageDiv.textContent = "Essayez encore !";
+        messageDiv.style.color = "#666";
+    }
+    
+};
+
+let nbEssaie = 0;
+let tentativeDiv = document.getElementById('tentative')
 const bouton = document.getElementById('bouton');
 const corpsTableau = document.getElementById('corpsTableau');
 const messageDiv = document.getElementById('message');
-tentativeDiv = document.getElementById('tentative')
+const lettres = decomposerMot(motAleatoire);
 const input = document.getElementById('monInput');
+
+/* fonction qui récupère le mot contenu dans l'input */
+
+
 
 
 bouton.addEventListener('click', () => {
@@ -54,41 +88,11 @@ bouton.addEventListener('click', () => {
     const nouvelleLigne = document.createElement('tr');
     
     let toutCorrect = true;
-    essaie.forEach((lettre, i) => {
-
-        const cellule = document.createElement('td');
-        cellule.textContent = lettre;
-
-
-        if (lettre === lettres[i]) {
-            cellule.classList.add('correct');
-            console.log(lettre + " - Bonne position");
-        } else if (lettres.includes(lettre)) {
-            cellule.classList.add('present');
-            console.log(lettre + " - Mauvaise position mais dans le mot");
-            toutCorrect = false;
-        } else {
-            cellule.classList.add('absent');
-            console.log(lettre + " - Pas dans le mot");
-            toutCorrect = false;
-        }
-
-        nouvelleLigne.appendChild(cellule);
-    });
+    verifierEssai(essaie, lettres, nouvelleLigne)
 
     corpsTableau.appendChild(nouvelleLigne);
 
-    if (toutCorrect) {
-        messageDiv.textContent = "🎉 Bravo ! Vous avez trouvé le mot !";
-        bouton.disabled = true;
-        input.disabled = true;
-
-    } else {
-        messageDiv.textContent = "Essayez encore !";
-        messageDiv.style.color = "#666";
-    }
-    
-    // Vider l'input
+    // Vider l'input après chaque essaie
     input.value = '';
 
 })
